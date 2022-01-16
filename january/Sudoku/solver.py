cdcb5505c1d297dba0b824dace4a77589e6c396e
Tymon Becella <tymon@becella.eu>
2022-01-16 04:30:31 +0100

    commit

--- january/Sudoku/solver.py
+++ january/Sudoku/solver.py
@@ -1,5 +1,3 @@
-from os import system
-
 def solve(sudoku):
     for y in range(len(sudoku.grid)):
         for x in range(len(sudoku.grid[0])):
@@ -8,13 +6,9 @@ def solve(sudoku):
                 if a == []:
                     return
                 for i in a:
-                    _ = system('cls')
-                    #sudoku.print()
                     sudoku.set_cell((x,y), i)
                     solve(sudoku)
                     if (sudoku.is_win()):
-                        _ = system('cls')
                         return
                     sudoku.set_cell((x,y), 0)
-                _ = system('cls')
                 return
