7b36dd14f981e24aff66ad3245fa63a1dcbe2ed8
Tymon Becella <tymon@becella.eu>
2022-01-16 04:29:40 +0100

    commit

--- january/Sudoku/solver.py
+++ january/Sudoku/solver.py
@@ -1,19 +1,20 @@
-from msilib.schema import Error
 from os import system
-from time import sleep
 
 def solve(sudoku):
     for y in range(len(sudoku.grid)):
         for x in range(len(sudoku.grid[0])):
             if sudoku.grid[y][x] == 0:
                 a = sudoku.possible((x,y))
-                _ = system('cls')
-                sudoku.print()
-                print(a)
-                print(x,y)
-                input()
+                if a == []:
+                    return
                 for i in a:
+                    _ = system('cls')
+                    #sudoku.print()
                     sudoku.set_cell((x,y), i)
-                    if not(sudoku.is_win()):
-                        solve(sudoku)
-                        sudoku.set_cell((x,y), 0)
+                    solve(sudoku)
+                    if (sudoku.is_win()):
+                        _ = system('cls')
+                        return
+                    sudoku.set_cell((x,y), 0)
+                _ = system('cls')
+                return
