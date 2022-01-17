93efe496a52ee21a0ad6e5997b5f1c69ff026a45
Tymon Becella <tymon@becella.eu>
2022-01-17 15:35:04 +0100

    commit

--- january/Sudoku/frontend.py
+++ january/Sudoku/frontend.py
@@ -1,4 +1,22 @@
+from generator import *
+from sudoku import *
 from tkinter import *
+
+def gen_btns(difficulty):
+    sudoku = generate(difficulty)
+    print(len(btns))
+    for btn in range(len(btns)):
+        btns[btn].text = sudoku.grid[btn//9][btn%9]
+        btns[btn].destroy()
+        #btns[-1].grid(row=btn//9, column=btn%9, sticky=N+S+E+W)
+        btns[-1].grid(row=btn//9, column=btn%9, sticky=N+S+E+W)
+
+def set_difficulty(diff):
+    difficulty = diff
+
+sudoku = Sudoku()
+btns = []
+difficulty = 'easy'
 
 root = Tk()
 root.resizable(False, False)
@@ -6,7 +24,10 @@ root.title("Sudoku")
 
 menu=Menu(root)
 file=Menu(menu)
-file.add_command(label="Generate", command= lambda: Ellipsis)
+file.add_command(label="Generate", command= lambda: gen_btns(difficulty))
+file.add_command(label="Easy", command= lambda: set_difficulty('easy'))
+file.add_command(label="Medium", command= lambda: set_difficulty('medium'))
+file.add_command(label="Hard", command= lambda: set_difficulty('hard'))
 file.add_command(label="Reset", command= lambda: Ellipsis)
 file.add_command(label="Exit", command= root.quit)
 
@@ -19,8 +40,8 @@ for y in range (9):
             color="light gray"
         else:
             color="white"
-        btn=Button(root, width = 10, height = 5, bg = color, text = 0, fg = "black", command = lambda: Ellipsis)   
-        btn.grid(row=y, column=x, sticky=N+S+E+W)
-        btn.grid(row=y, column=x, sticky=N+S+E+W)
+        btns.append(Button(root, width = 10, height = 5, bg = color, text = 0, fg = "black", font=('Courier', 10), command = lambda: Ellipsis))
+        btns[-1].grid(row=y, column=x, sticky=N+S+E+W)
+        btns[-1].grid(row=y, column=x, sticky=N+S+E+W)
 
 root.mainloop()
